2c119f1a011c593e4f1b2f95acfa0ab33092158b
JunjoFor <castillooton@gmail.com>
2024-10-29 19:20:50 +0100

    Plotting doesn't work

--- example.py
+++ example.py
@@ -5,12 +5,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# sns.set_theme(style="whitegrid")
-
 # Parsing folders
 
 samplesUsed = ["fir", "bfs"]
-secondDir = ["default"]
+secondDir = ["default", "TLB_noMisses"]
 metrics = {}
 kernels_time = {}
 
@@ -20,9 +18,9 @@ for dir in secondDir:
         kernels_time[dir] = {}
 
 for dir in secondDir:
+    # kernels_time[dir]['Benchmark'] = samplesUsed
     for sample in samplesUsed:
         metrics[dir][sample] = pd.read_csv("../mgpusim/samples/" + sample + "/" + dir + "/metrics.csv")
-        kernels_time[dir]['Benchmark'] = 'Kenel Time'
 
 for single_m, multi_metric in metrics.items():
     for metric in multi_metric:
@@ -30,12 +28,12 @@ for single_m, multi_metric in metrics.items():
         aux = dict_metrics.get(metric)
         for i, j in aux.iterrows():
             if j[' what'] == ' kernel_time':
-                kernels_time[single_m][metric] = j[' value']
+                kernels_time[single_m][metric] = [j[' value']]
 
-data_frame_kernel = {}
-for dir in secondDir:
-    data_frame_kernel[dir] = pd.Series(kernels_time.get(dir))
+# No funciona bien
+data_serie_kernel = pd.DataFrame(kernels_time)
+data_frame_default = pd.DataFrame(kernels_time.get("default"))
+data_frame_TLB_noMisses = pd.DataFrame(kernels_time.get("TLB_noMisses"))
+print(data_serie_kernel)
 
-for frame in data_frame_kernel:
-    print(frame)
-    print(data_frame_kernel.get(frame))
+sns.barplot(data = data_serie_kernel, x='default', y='TLB_noMisses')
