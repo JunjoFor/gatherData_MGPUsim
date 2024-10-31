2526d9d5b046bc5a0c860e40accaa228f0c308ed
JunjoFor <castillooton@gmail.com>
2024-10-31 15:41:12 +0100

    Fixing path 2.0

--- compileAndExecute.py
+++ compileAndExecute.py
@@ -5,8 +5,7 @@ sample_path = "/gpfs/projects/bsc18/bsc125019/mgpusim/samples/"
 mgpu_args = " -timing -report-all -unified-gpus=1,2,3,4 -use-unified-memory"
 samples_paths = os.listdir(sample_path)
 
-os.system("cd " + sample_path)
 
 for sample in samples_paths:
     if sample != "runner":
-        os.system("module purge ; module load spack ; source ../spack/share/spack/setup-env.sh ; spack load golang ; " + "cd ./" + sample + "; go build ; sbatch --job-name=" + sample + " ./" + sample + mgpu_args)
+        os.system( "cd " + sample_path + "; module purge ; module load spack ; source /gpfs/projects/bsc18/bsc125019/spack/share/spack/setup-env.sh ; spack load golang ; " + "cd ./" + sample + "; go build ; sbatch --job-name=" + sample + " ./" + sample + mgpu_args)
