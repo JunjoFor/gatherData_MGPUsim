77fc4cef801564682c82824b894a0ec796315318
JunjoFor <castillooton@gmail.com>
2024-10-31 14:11:23 +0100

    Plotting kernel_Time

--- example.py
+++ example.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 # Parsing folders
 
-samplesUsed = ["fir", "bfs"]
+samplesUsed = ["bfs", "fir"]
 secondDir = ["default", "TLB_noMisses"]
 metrics = {}
 kernels_time = {}
@@ -22,13 +22,13 @@ for dir in secondDir:
     for sample in samplesUsed:
         metrics[dir][sample] = pd.read_csv("../mgpusim/samples/" + sample + "/" + dir + "/metrics.csv")
 
-for single_m, multi_metric in metrics.items():
-    for metric in multi_metric:
-        dict_metrics = metrics.get(single_m)
-        aux = dict_metrics.get(metric)
+for directory, samples in metrics.items():
+    for sample in samples:
+        dict_metrics = metrics.get(directory)
+        aux = dict_metrics.get(sample)
         for i, j in aux.iterrows():
             if j[' what'] == ' kernel_time':
-                kernels_time[single_m][metric] = [j[' value']]
+                kernels_time[directory][sample] = float(j[' value'])
 
 # No funciona bien
 data_serie_kernel = pd.DataFrame(kernels_time)
@@ -36,4 +36,29 @@ data_serie_kernel = pd.DataFrame(kernels_time)
 # data_frame_TLB_noMisses = pd.DataFrame(kernels_time.get("TLB_noMisses"))
 print(data_serie_kernel)
 
-# sns.barplot(data = data_serie_kernel, x='default', y='TLB_noMisses')
+# sns.barplot(data = data_serie_kernel, 
+#              # x='default', y='TLB_noMisses'
+#             )
+fig, ax = plt.subplots(constrained_layout=True)
+
+key_list = data_serie_kernel.columns.to_list()
+xticklabels = data_serie_kernel.index.to_list()
+
+cat_spacing = 0.1
+bar_width = (1 - cat_spacing) / len(key_list)
+
+index = np.arange(len(xticklabels))
+
+# Plot bars
+for i, key in enumerate(key_list):
+    value = data_serie_kernel[key]
+    ax.bar(index + i * bar_width, value, width=bar_width, label=key)
+
+ax.set_ylim(0, 0.000000004500)
+ax.set_xticks(index + bar_width / 2 * (len(key_list) - 1))
+ax.set_xticklabels(xticklabels)
+ax.set_xlabel('Samples')
+ax.set_ylabel('Kernel Time')
+ax.legend(title='Directories')
+
+plt.show()
