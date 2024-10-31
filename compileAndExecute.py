08b409cf20a8621934ad423a23ae4961a6884775
JunjoFor <castillooton@gmail.com>
2024-10-31 16:06:19 +0100

    Creation of run.sh

--- compileAndExecute.py
+++ compileAndExecute.py
@@ -6,6 +6,9 @@ mgpu_args = " -timing -report-all -unified-gpus=1,2,3,4 -use-unified-memory"
 samples_paths = os.listdir(sample_path)
 
 
+
 for sample in samples_paths:
     if sample != "runner":
-        os.system( "cd " + sample_path + "; module purge ; module load spack ; source /gpfs/projects/bsc18/bsc125019/spack/share/spack/setup-env.sh ; spack load golang ; " + "cd ./" + sample + "; go build ; srun -A bsc18 -q gp_bsccs --job-name=" + sample + " ./" + sample + mgpu_args)
+        sbatch_file = open(sample_path + sample + "/run.sh", 'w')
+        sbatch_file.write("#!/bin/bash\n#SBATCH -n 1\n#SBATCH -c 2\n#SBATCH --ntasks=4\n#SBATCH --job-name=" + sample +"\nexport SRUN_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK}\n ./" + sample + mgpu_args)
+        os.system( "cd " + sample_path + "; module purge ; module load spack ; source /gpfs/projects/bsc18/bsc125019/spack/share/spack/setup-env.sh ; spack load golang ; " + "cd ./" + sample + "; go build ; sbatch -A bsc18 -q gp_bsccs run.sh")
