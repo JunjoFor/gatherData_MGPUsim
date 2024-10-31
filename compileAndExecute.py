21c1baad99ed7e8fb2a7eb0896ff5a7279eb7adb
JunjoFor <castillooton@gmail.com>
2024-10-31 16:08:08 +0100

    fix open

--- compileAndExecute.py
+++ compileAndExecute.py
@@ -11,4 +11,5 @@ for sample in samples_paths:
     if sample != "runner":
         sbatch_file = open(sample_path + sample + "/run.sh", 'w')
         sbatch_file.write("#!/bin/bash\n#SBATCH -n 1\n#SBATCH -c 2\n#SBATCH --ntasks=4\n#SBATCH --job-name=" + sample +"\nexport SRUN_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK}\n ./" + sample + mgpu_args)
+        sbatch_file.close()
         os.system( "cd " + sample_path + "; module purge ; module load spack ; source /gpfs/projects/bsc18/bsc125019/spack/share/spack/setup-env.sh ; spack load golang ; " + "cd ./" + sample + "; go build ; sbatch -A bsc18 -q gp_bsccs run.sh")
