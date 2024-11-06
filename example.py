ecebce7cf3383c709257bfbb1bfb7c5e45060c2a
JunjoFor <castillooton@gmail.com>
2024-11-06 15:11:17 +0100

    testing

--- example.py
+++ example.py
@@ -87,10 +87,10 @@ index = np.arange(len(xticklabels))
 # Plot bars
 for i, key in enumerate(key_list):
     value = sorted_df[key]
-    ax.bar(index + i * bar_width, value, width=bar_width, label=key)
+    ax.bar(index + i * bar_width, value, width=bar_width*2, label=key)
 
 # ax.set_ylim(0, 0.000000004500)
-ax.set_xticks(index + bar_width + 5 / 2 * (len(key_list) - 1))
+ax.set_xticks(index + bar_width / 2 * (len(key_list) - 1))
 ax.set_xticklabels(xticklabels)
 ax.set_xlabel('Samples')
 ax.set_ylabel('Kernel Time')
