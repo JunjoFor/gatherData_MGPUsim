0a7aa67370951a04c6d49a29543ade205a25a432
JunjoFor <castillooton@gmail.com>
2024-11-06 13:37:03 +0100

    fix

--- example.py
+++ example.py
@@ -32,9 +32,10 @@ for directory, samples in metrics.items():
         for i, j in aux.iterrows():
             if j[' what'] == ' kernel_time' and j[' where'] == ' Driver':
                 kernels_time[directory][sample] = float(j[' value'])
-            if directory == "default" and j[' what'] == ' cu_inst_count':
+            print("El valor de i es: ", i)
+            if i == "default" and j[' what'] == ' cu_inst_count':
                 insts = insts + float(j[' value'])
-            if directory =="default" and ("L1VTLB[0]" in  j[' where']) and j[' what'] == 'miss':
+            if i =="default" and ("L1VTLB[0]" in  j[' where']) and j[' what'] == 'miss':
                 tlb_misses = tlb_misses + float(j[' value'])
         kernels_time["TLB_misses"] = tlb_misses
         kernels_time["Instructions"] = insts
