78cf54c7f66400b6eff62e5f0794e85a31096e8b
JunjoFor <castillooton@gmail.com>
2024-11-06 13:51:23 +0100

    debug

--- example.py
+++ example.py
@@ -33,14 +33,13 @@ for directory, samples in metrics.items():
             if j[' what'] == ' kernel_time' and j[' where'] == ' Driver':
                 kernels_time[directory][sample] = float(j[' value'])
             if directory == "default" and j[' what'] == ' cu_inst_count':
-                insts = insts + float(j[' value'])
+                kernels_time["Instructions"][sample] = kernels_time["Instructions"] + float(j[' value'])
             component = j[' where'].split(".")
             if len(component) > 2:
+                print("Este es el componente : ", component)
                 if directory =="default" and component[2] == "L1VTLB[0]" and j[' what'] == 'miss':
                     print("El valor de value: in misses", float(j[' value']))
-                tlb_misses = tlb_misses + float(j[' value'])
-            kernels_time["TLB_misses"] = tlb_misses
-            kernels_time["Instructions"] = insts
+                kernels_time["TLB_misses"][sample] = kernels_time["TLB_misses"] + float(j[' value'])
 
 # No funciona bien
 data_serie_kernel = pd.DataFrame(kernels_time)
