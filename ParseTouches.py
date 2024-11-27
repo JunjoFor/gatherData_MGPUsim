be5f60b2e33c4065ea791a5427b77b0309cd3338
JunjoFor <castillooton@gmail.com>
2024-11-27 11:33:47 +0100

    fixing ParseTouches

--- ParseTouches.py
+++ ParseTouches.py
@@ -4,8 +4,8 @@ import os
 f = open(sys.argv[1] + "/mem.trace", "r")
 outF = open(sys.argv[1] + "page_touches.txt" , "w")
 
-line = f.readline()
-if "No_touches" in line:
-    outF.write(line)
+for line in f:
+    if "No_touches" in line:
+        outF.write(line)
 outF.close()
 f.close()
